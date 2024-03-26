116c3d26dfd7a85acab15cd966e3f6fd8f2667d6
Kamal Singh <kamalsingh.l@prodapt.com>
2024-03-26 09:23:15 +0530

    Updated the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,29 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+import "./Button.css";
 
 export const Button = ({ title }) => (
   <div style={{ width: "170px", height: "47px", marginRight: "32px" }}>
-    <StyledButton>{title}</StyledButton>
+    <button className="feedback_button">{title}</button>
   </div>
 );
-
-const StyledButton = styled.button`
-  font-family: "Poppins";
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  background-color: #121212;
-  color: #34c94b;
-  padding: 10px 16px;
-  border: 1px solid #121212;
-  border-radius: 12px;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 27px;
-  letter-spacing: 0.25px;
-  &:hover {
-    cursor: pointer;
-  }
-`;
